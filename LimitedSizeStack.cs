2dc9cdfb2e1b498da314cd5ea67358c151a9b887
Popov Vitaliy <vpopov@65apps.ru>
2023-04-07 22:59:19 +0500

    Добавлена кнопка moveUp

---
+++ LimitedSizeStack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LimitedSizeStack;
+
+public class LimitedSizeStack<T>
+{
+    private readonly LinkedList<T> store;
+    private readonly int undoLimit;
+
+    public LimitedSizeStack(int undoLimit)
+    {
+        store = new LinkedList<T>();
+        this.undoLimit = undoLimit;
+    }
+
+    public void Push(T item)
+    {
+        if (undoLimit == 0) 
+            return;
+
+        if (store.Count == undoLimit)
+            store.RemoveLast();
+
+        store.AddFirst(item);
+    }
+
+    public T Pop()
+    {
+        var value = store.First.Value;
+        store.RemoveFirst();
+
+        return value;
+    }
+    
+    public int Count => store.Count;
+}
---
+++ ListModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LimitedSizeStack;
+
+public class ListModel<TItem>
+{
+	public List<TItem> Items { get; }
+	public int UndoLimit;
+	private LimitedSizeStack<Action> actions;
+
+	public ListModel(int undoLimit) : this(new List<TItem>(), undoLimit)
+	{
+	}
+
+	public ListModel(List<TItem> items, int undoLimit)
+	{
+		Items = items;
+		UndoLimit = undoLimit;
+		actions = new LimitedSizeStack<Action>(undoLimit);
+	}
+
+	public void AddItem(TItem item)
+	{
+		actions.Push(new AddAction(Items, item));
+		Items.Add(item);
+	}
+
+	public void RemoveItem(int index)
+	{
+		actions.Push(new RemoveAction(Items, Items[index], index));
+		Items.RemoveAt(index);
+	}
+
+	public bool CanUndo()
+	{
+		return actions.Count > 0;
+	}
+
+	public void Undo()
+	{
+		actions.Pop().DoUndoAction();
+	}
+	
+	public void MoveUpItem(int index)
+	{
+		var thisItem = Items[index];
+		var nextItem = Items[index - 1];
+
+		Items[index] = nextItem;
+		Items[index - 1] = thisItem;
+	}
+	
+	private abstract class Action
+	{
+		protected readonly List<TItem> items;
+		protected readonly TItem item;
+
+		protected Action(List<TItem> container, TItem item)
+		{
+			items = container;
+			this.item = item;
+		}
+
+		public abstract void DoUndoAction();
+	}
+	
+	private class AddAction : Action
+	{
+		public AddAction(List<TItem> container, TItem item) : base(container, item)
+		{
+		}
+		
+		public override void DoUndoAction()
+		{
+			items.RemoveAt(items.Count - 1);
+		}
+	}
+	
+	private class RemoveAction : Action
+	{
+		private readonly int itemIndex;
+		
+		public RemoveAction(List<TItem> container, TItem item, int itemIndex) : base(container, item)
+		{
+			this.itemIndex = itemIndex;
+		}
+		
+		public override void DoUndoAction()
+		{
+			items.Insert(itemIndex, item);
+		}
+	}
+}
